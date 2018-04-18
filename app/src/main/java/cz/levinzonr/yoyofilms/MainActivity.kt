69f1863fd5f6bd81fc5d5ec68e96d5a4daf0ad9f
Roman Levinzon <levinzon.roman@gmail.com>
2018-04-18 20:35:39 +0200

    Navigation menu implemented

--- app/src/main/java/cz/levinzonr/yoyofilms/MainActivity.kt
+++ app/src/main/java/cz/levinzonr/yoyofilms/MainActivity.kt
@@ -2,33 +2,70 @@ package cz.levinzonr.yoyofilms
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import cz.levinzonr.yoyofilms.view.FavoritesFragment
+import cz.levinzonr.yoyofilms.view.TrendingFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
+            R.id.navigation_favorites -> {
+                view_pager.currentItem = 1
+                true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
+            R.id.navigation_movies -> {
+                view_pager.currentItem = 0
+                true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
+            else -> {
+                true
             }
         }
-        false
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(FavoritesFragment())
+        adapter.addFragment(TrendingFragment())
+        view_pager.adapter = adapter
+        view_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            private var previous: Int = 0
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                navigation.menu.getItem(previous).isChecked = false
+                navigation.menu.getItem(position).isChecked = true
+                previous = position
+            }
+        })
     }
+
+    inner class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        private val fragments = ArrayList<Fragment>()
+
+        fun addFragment(fragment: Fragment) {
+            fragments.add(fragment)
+        }
+
+        override fun getItem(position: Int): Fragment = fragments[position]
+
+        override fun getCount(): Int = fragments.size
+    }
+
 }
