6b1506fcbaf07b07c8a6507f5018325dad693f1f
Roman Levinzon <levinzon.roman@gmail.com>
2018-04-18 20:45:26 +0200

    Movie model added

---
+++ app/src/main/java/cz/levinzonr/yoyofilms/model/Movies.kt
@@ -0,0 +1,10 @@
+package cz.levinzonr.yoyofilms.model
+
+class Movies {
+    var id: Int = -1
+    var budget: Int = -1
+    var releaseDate: String = String()
+    var title: String = String()
+    var overview: String? = null
+
+}
---
+++ app/src/main/java/cz/levinzonr/yoyofilms/model/RemoteDataSource.kt
@@ -0,0 +1,10 @@
+package cz.levinzonr.yoyofilms.model
+
+class RemoteDataSource {
+
+    companion object {
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val API_KEY = "4cb1eeab94f45affe2536f2c684a5c9e"
+    }
+
+}
